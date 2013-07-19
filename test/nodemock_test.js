9dda2074fcac7a724985169a446e0d656addc184
adampickeral <adam.pickeral@rackspace.com>
2013-07-19 16:00:19 -0400

    first passing test

---
+++ test/nodemock_test.js
@@ -0,0 +1,8 @@
+// var nodemock = require('../lib/nodemock');
+var should = require('should');
+
+describe('nodemock', function () {
+  it('passes a test', function () {
+    true.should.be.true;
+  });
+});
