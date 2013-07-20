be63f2e4a42ba0ceef14a705ead85654966f1cd4
adampickeral <adam.pickeral@rackspace.com>
2013-07-19 23:34:19 -0400

    argument object equality

--- test/nodemock_test.js
+++ test/nodemock_test.js
@@ -52,6 +52,19 @@ describe('nodemock', function () {
     mockDummyObject.testFunction.should.haveBeenCalledWith('someString', 'anotherString', 3);
   });
 
+  it('asserts that a function was called with an object', function () {
+    var DummyObject, mockDummyObject;
+
+    DummyObject = function () {};
+    DummyObject.prototype.testFunction = function () {};
+
+    mockDummyObject = nodemock.mock(DummyObject);
+
+    mockDummyObject.testFunction({ 'a': 'b', 'c': 1 });
+
+    mockDummyObject.testFunction.should.haveBeenCalledWith({ 'a': 'b', 'c': 1 });
+  });
+
   it('asserts that a function was not called with a string', function () {
     var DummyObject, mockDummyObject;
 
@@ -75,7 +88,32 @@ describe('nodemock', function () {
 
     mockDummyObject.testFunction('someString');
     mockDummyObject.testFunction('someString', 5, 'anotherString');
-
     mockDummyObject.testFunction.should.not.haveBeenCalledWith('someString', 1, 'anotherString');
+  });
+
+  it('asserts that strict equality is enforced', function () {
+    var DummyObject, mockDummyObject;
+
+    DummyObject = function () {};
+    DummyObject.prototype.testFunction = function () {};
+
+    mockDummyObject = nodemock.mock(DummyObject);
+
+    mockDummyObject.testFunction('1');
+
+    mockDummyObject.testFunction.should.not.haveBeenCalledWith(1);
+  });
+
+  it('asserts that a function was not called with an object', function () {
+    var DummyObject, mockDummyObject;
+
+    DummyObject = function () {};
+    DummyObject.prototype.testFunction = function () {};
+
+    mockDummyObject = nodemock.mock(DummyObject);
+
+    mockDummyObject.testFunction({ 'a': 'b', 'c': 1, 'd': 2 });
+
+    mockDummyObject.testFunction.should.not.haveBeenCalledWith({ 'a': 'b', 'c': 2 });
   });
 });
